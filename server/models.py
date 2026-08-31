3120bbc91c3ee156d5804cc73caf241798945884
Elvis Chege <elvischege98@gmail.com>
2026-08-31 11:09:26 +0300

    Finished creating  workout model

--- server/models.py
+++ server/models.py
@@ -11,3 +11,11 @@ class Exercise(db.Model):
     category = db.Column(db.String(100), nullable=False)
     equipment_needed = db.Column(db.Boolean, nullable=False)
 
+
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text)

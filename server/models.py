61f3ae284ce316b952092eadfc8ce15ad9d11d82
Elvis Chege <elvischege98@gmail.com>
2026-08-31 11:04:07 +0300

    Finished creating exercise model

--- server/models.py
+++ server/models.py
@@ -1,5 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+
 db = SQLAlchemy()
 
-# Define Models here
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    equipment_needed = db.Column(db.Boolean, nullable=False)
+

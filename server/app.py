a08bd784ebdf88b6edf6dc9afefea73582b57b74
Elvis Chege <elvischege98@gmail.com>
2026-09-01 10:00:27 +0300

    POST functionality added to app.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response,jsonify
+from flask import Flask, make_response,jsonify,request
 from flask_migrate import Migrate
 
 from models import *
@@ -68,6 +68,36 @@ def get_workout(id):
         "duration_minutes": workout.duration_minutes,
         "notes": workout.notes
     })
+
+#Get all exercises for a specific workout
+@app.route("/exercises", methods=["POST"])
+def create_exercise():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "Invalid input"}), 400
+
+    required_fields = ["name", "category", "equipment_needed"]
+
+    for field in required_fields:
+        if field not in data:
+            return jsonify({"error": f"Missing field: {field}"}), 400
+
+    exercise = Exercise(
+        name=data["name"],
+        category=data["category"],
+        equipment_needed=data["equipment_needed"]
+    )
+
+    db.session.add(exercise)
+    db.session.commit()
+
+    return jsonify({
+        "id": exercise.id,
+        "name": exercise.name,
+        "category": exercise.category,
+        "equipment_needed": exercise.equipment_needed
+    }), 201
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
